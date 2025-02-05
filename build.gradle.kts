bf884a63e816fbd64dcd8f804b75d2548f737d51
tmd2311 <dung.kayc@gmail.com>
2025-02-06 02:24:50 +0700

    Call the translation and dictionary api

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 
---
+++ src/main/java/utc/englishlearning/Encybara/domain/response/dictionary/Definition.java
@@ -0,0 +1,11 @@
+package utc.englishlearning.Encybara.domain.response.dictionary;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Definition {
+    private String definition;
+    private String example;
+}
